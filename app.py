c1e98157c3f501454951e341e14109c23a3f6e97
Isaac3060 <isaacmanuel2299@gmail.com>
2020-05-07 19:34:27 +0000

    first commit

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ import random
 import math
 
 # your code here
+
+who = ['the dog','my granma','his turtle','my bird']
+what = ['eat','pissed','crushed','broked']
+when = ['before the class','right in time','when I finished','during my lunch','while I was praying']
+
+def excuse (a,b,c):
+    x = random.randint(0,len(a)-1)
+    y = random.randint(0,len(b)-1)
+    z = random.randint(0,len(c)-1)
+
+    print(x,y,z)
+    return a[x]+" "+b[y] + " "+c[z]
+
+print(excuse(who,what,when))
